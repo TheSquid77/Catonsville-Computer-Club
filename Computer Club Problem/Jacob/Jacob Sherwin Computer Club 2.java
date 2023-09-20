d1f980c423e2dcf657ce5732b486ff1747b87c8c
TheSquid77 <44305994+TheSquid77@users.noreply.github.com>
2023-09-20 15:03:38 -0400

    Add files via upload

---
+++ Computer Club Problem/Jacob/Jacob Sherwin Computer Club 2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+        int num = 9 + 20;
+
+        num -= 23;
+
+        System.out.println(num);
+        
+    }
+}
